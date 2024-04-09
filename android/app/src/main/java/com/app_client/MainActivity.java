4fb844f4c5914de90a4558b922bea8a6b885802a
Iago Saraiva <iago.saraiva@pmovil.com.br>
2024-04-09 14:27:35 -0300

    package rename

--- android/app/src/main/java/com/app_client/MainActivity.java
+++ android/app/src/main/java/com/app_client/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "app_client";
+    return "Salty point";
   }
 
   @Override
